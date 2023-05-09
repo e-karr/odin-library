5f93e6242d1233309c9b7f3b5a0f087eb6bdadc8
Elizabeth Karr <e.karr93@icloud.com>
2023-05-09 15:46:16 -0500

    write function to display library; link .js file to html

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let myLibrary = [];
+let myLibrary = [
+  { title: 'Hello', author: 'World', pages: 120, read: true },
+  { title: 'World', author: 'Hello', pages: 200, read: false },
+  { title: 'Dog Days', author: 'Hammond Karr', pages: 500, read: true },
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,3 +15,24 @@ function addBookToLibrary(title, author, pages, read) {
   let newBook = Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
+
+let libraryContainer = document.getElementById('library-container');
+
+function displayLibrary(libraryArray) {
+  let libraryHTML = '';
+  for (let book of libraryArray) {
+    libraryHTML += `
+      <div>
+        <h3>Title: ${book.title}</h3>
+        <h4>Author: ${book.author}</h4>
+        <p>Pages: ${book.pages}</p>
+        <button type=button>${book.read ? 'Read' : 'Not Read'}</button>
+        <button>Remove</button>
+      </div>
+    `;
+  }
+
+  return libraryHTML;
+}
+
+libraryContainer.innerHTML = displayLibrary(myLibrary);
