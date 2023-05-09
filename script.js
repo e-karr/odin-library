db1bd2285fd22a4bad96ca2cf12d9d505838aebd
Elizabeth Karr <e.karr93@icloud.com>
2023-05-09 15:26:05 -0500

    write addBookToLibrary function

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
 
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.read ? "read" : "not read yet"
-    }`;
-  };
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
