082347768d3a5068d1f30ba4601f42ca06f5acf3
Elizabeth Karr <e.karr93@icloud.com>
2023-06-22 11:18:04 -0500

    refactor to use classes

--- script.js
+++ script.js
@@ -1,97 +1,169 @@
 /* eslint-disable require-jsdoc */
-// Global variables
-const libraryContainer = document.getElementById('library-container');
-const addBookButton = document.getElementById('add-book');
-const addBookForm = document.querySelector('form');
-const cancelAddBookButton = document.getElementById('cancel-add');
-const emptyLibrary = document.getElementById('empty');
 
-const myLibrary = [];
+class Book {
+  title;
+  author;
+  pages;
+  readStatus;
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-}
-
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  displayLibrary(myLibrary);
-}
-
-function changeReadStatus(index) {
-  const readStatusButton = document.getElementById(`read-button${index}`);
-  if (myLibrary[index].read) {
-    myLibrary[index].read = false;
-    readStatusButton.classList.remove('read-status');
-    readStatusButton.classList.add('not-read');
-  } else {
-    myLibrary[index].read = true;
-    readStatusButton.classList.remove('not-read');
-    readStatusButton.classList.add('read-status');
+  constructor(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
   }
 
-  displayLibrary(myLibrary);
+  changeReadStatus() {
+    this.readStatus = this.readStatus ? false : true;
+  }
 }
 
-function displayLibrary(libraryArray) {
-  let libraryHTML = '';
+class Library {
+  books;
 
-  if (libraryArray.length === 0) {
-    emptyLibrary.style.display = 'block';
-    libraryContainer.style.display = 'none';
-  } else {
-    libraryContainer.style.display = 'grid';
+  constructor(books = []) {
+    this.books = books;
+  }
 
-    for (let i = 0; i < libraryArray.length; i++) {
-      libraryHTML += `
-        <div class=book>
-          <h3>${libraryArray[i].title}</h3>
-          <h4>by ${libraryArray[i].author}</h4>
-          <p>${libraryArray[i].pages} pages</p>
-          <div>
-            <button id=read-button${i} class=${
-        libraryArray[i].read ? 'read-status' : 'not-read'
-      } type=button onclick=changeReadStatus(${i})>${
-        libraryArray[i].read ? 'Read' : 'Not Read'
-      }</button>
-            <button class=remove onclick=removeBook(${i})>Remove</button>
-          </div>
-        </div>
-      `;
+  addBook(title, author, pages, readStatus) {
+    this.books.push(new Book(title, author, pages, readStatus));
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+  }
+}
+
+function ScreenController() {
+  const library = new Library([new Book('Test', 'Jane Doe', 250, false)]);
+
+  const emptyLibrary = document.getElementById('empty');
+  const libraryContainer = document.getElementById('library-container');
+
+  const displayLibrary = () => {
+    // clear the library
+    libraryContainer.textContent = '';
+
+    if (library.books.length == 0) {
+      emptyLibrary.style.visibility = 'visible';
+      libraryContainer.style.visibility = 'hidden';
+    } else {
+      for (let i = 0; i < library.books.length; i++) {
+        const book = document.createElement('div');
+        book.classList.add('book');
+
+        const title = document.createElement('h3');
+        title.textContent = library.books[i].title;
+        book.appendChild(title);
+
+        const author = document.createElement('h4');
+        author.textContent = library.books[i].author;
+        book.appendChild(author);
+
+        const pages = document.createElement('p');
+        pages.textContent = `${library.books[i].pages} pages`;
+        book.appendChild(pages);
+
+        const buttons = document.createElement('div');
+
+        const readStatusButton = document.createElement('button');
+        readStatusButton.classList.add(
+          library.books[i].readStatus ? 'read' : 'not-read'
+        );
+        readStatusButton.textContent = library.books[i].readStatus
+          ? 'Read'
+          : 'Not Read';
+
+        readStatusButton.addEventListener(
+          'click',
+          library.books[i].changeReadStatus
+        );
+
+        const removeBookButton = document.createElement('button');
+        removeBookButton.classList.add('remove');
+        removeBookButton.textContent = 'Remove';
+        removeBookButton.addEventListener('click', library.removeBook(i));
+
+        buttons.appendChild(readStatusButton);
+        buttons.appendChild(removeBookButton);
+        book.appendChild(buttons);
+
+        libraryContainer.appendChild(book);
+      }
+
+      emptyLibrary.style.visibility = 'hidden';
+      libraryContainer.style.visibility = 'visible';
     }
-    emptyLibrary.style.display = 'none';
-    libraryContainer.innerHTML = libraryHTML;
-  }
+  };
+
+  // initial library display
+  displayLibrary();
 }
 
-window.addEventListener('load', () => displayLibrary(myLibrary));
+ScreenController();
 
-addBookButton.addEventListener('click', () => {
-  addBookForm.style.visibility = 'visible';
-});
+// Global variables
+// const libraryContainer = document.getElementById('library-container');
+// const addBookButton = document.getElementById('add-book');
+// const addBookForm = document.querySelector('form');
+// const cancelAddBookButton = document.getElementById('cancel-add');
+// const emptyLibrary = document.getElementById('empty');
 
-cancelAddBookButton.addEventListener('click', () => {
-  addBookForm.style.visibility = 'hidden';
-  addBookForm.reset();
-});
+// const myLibrary = [];
 
-addBookForm.addEventListener('submit', (event) => {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = parseInt(document.getElementById('pages').value);
-  const read = document.getElementById('read').checked;
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
 
-  addBookToLibrary(title, author, pages, read);
-  displayLibrary(myLibrary);
+// function addBookToLibrary(title, author, pages, read) {
+//   const newBook = new Book(title, author, pages, read);
+//   myLibrary.push(newBook);
+// }
 
-  addBookForm.style.visibility = 'hidden';
-  addBookForm.reset();
-  event.preventDefault();
-});
+// function removeBook(index) {
+//   myLibrary.splice(index, 1);
+//   displayLibrary(myLibrary);
+// }
+
+// function changeReadStatus(index) {
+//   const readStatusButton = document.getElementById(`read-button${index}`);
+//   if (myLibrary[index].read) {
+//     myLibrary[index].read = false;
+//     readStatusButton.classList.remove('read-status');
+//     readStatusButton.classList.add('not-read');
+//   } else {
+//     myLibrary[index].read = true;
+//     readStatusButton.classList.remove('not-read');
+//     readStatusButton.classList.add('read-status');
+//   }
+
+//   displayLibrary(myLibrary);
+// }
+
+// window.addEventListener('load', () => displayLibrary(myLibrary));
+
+// addBookButton.addEventListener('click', () => {
+//   addBookForm.style.visibility = 'visible';
+// });
+
+// cancelAddBookButton.addEventListener('click', () => {
+//   addBookForm.style.visibility = 'hidden';
+//   addBookForm.reset();
+// });
+
+// addBookForm.addEventListener('submit', (event) => {
+//   const title = document.getElementById('title').value;
+//   const author = document.getElementById('author').value;
+//   const pages = parseInt(document.getElementById('pages').value);
+//   const read = document.getElementById('read').checked;
+
+//   addBookToLibrary(title, author, pages, read);
+//   displayLibrary(myLibrary);
+
+//   addBookForm.style.visibility = 'hidden';
+//   addBookForm.reset();
+//   event.preventDefault();
+// });
